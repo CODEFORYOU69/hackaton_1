46848f5bc2f05b306cd97be53c0b48fa099c3147
gaellegoyon <gaellegoyon.pro@gmail.com>
2022-11-25 11:26:39 +0100

    final

--- frontend/src/components/BestAtt.jsx
+++ frontend/src/components/BestAtt.jsx
@@ -1,189 +1,84 @@
 /* eslint-disable */
-import "../style/BestAtt.css";
-import * as React from "react";
-import { styled } from "@mui/material/styles";
+
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../style/rub.css";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-function BestAtt() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+const BestAtt = () => {
   return (
-    <div>
-      <div className="container">
-        <h2 className="cat-title">TOP 3 DES PARCS D'ATTRACTION</h2>
-        <div className="line-container">
-          <div className="line" />
-          <div className="img-wheel" />
-          <div className="line" />
+    <div className="rub-container">
+      <div className="back-container">
+        <div className="txt-container">
+          <h2 className="rub-title1">Top 3</h2>
+          <h2 className="rub-title2">des parcs</h2>
         </div>
         <div className="card-container">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
+          <Card
+            sx={{ width: "20vw", height: "45vh", borderRadius: "2rem" }}
+            className="card-cont3"
+          >
+            <CardActionArea>
+              <CardMedia className="img-card3" />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="rub-tit1"
+                >
+                  Europapark
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <span className="rub-tit">2ème</span>
             </CardActions>
           </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
+          <Card
+            sx={{ width: "20vw", height: "45vh", borderRadius: "2rem" }}
+            className="card-cont1"
+          >
+            <CardActionArea>
+              <CardMedia className="img-card1" />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="rub-tit1"
+                >
+                  Disneyland Paris
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <span className="rub-tit">1er</span>
             </CardActions>
           </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
+          <Card
+            sx={{ width: "20vw", height: "45vh", borderRadius: "2rem" }}
+            className="card-cont2"
+          >
+            <CardActionArea>
+              <CardMedia className="img-card2" />
+              <CardContent>
+                <span className="rub-tit1">Disneyland Orlando</span>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <span className="rub-tit">3ème</span>
             </CardActions>
           </Card>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default BestAtt;
--- frontend/src/pages/FindPark.jsx
+++ frontend/src/pages/FindPark.jsx
@@ -6,20 +6,27 @@ import ParkCard from "../components/ParkCard";
 import Map from "./Map";
 import "../style/park_card.css";
 import Filter from "@components/Filter";
+import L from "leaflet";
 
 function FindPark({ parks }) {
-  const [getID, setGetID] = useState();
-  const [isClicked, setIsClicked] = useState(false);
+  const [getID, setGetID] = useState([]);
+  const [layerGroup] = useState(L.layerGroup()); // ajout calque map
 
   const handleClick = () => {
-    setIsClicked(!isClicked);
+    layerGroup.clearLayers();
     console.log("hello");
   };
+
   return (
     <div className="find">
       <Filter />
-      <ParkCard parks={parks} setGetID={setGetID} handleClick={handleClick} />
-      <Map getID={getID} isClicked={isClicked} />
+      <ParkCard
+        parks={parks}
+        setGetID={setGetID}
+        handleClick={handleClick}
+        getID={getID}
+      />
+      <Map getID={getID} handleClick={handleClick} layerGroup={layerGroup} />
     </div>
   );
 }
